9faa16fbaff5177aac5a31ae2fc2b5dc52ca807a
Bibek Panthi <bpanthi977@gmail.com>
2017-04-03 17:47:24 +0545

    index page added

--- heroku-start.lisp
+++ heroku-start.lisp
@@ -3,4 +3,10 @@
 
 ;; Start the server
 (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port (parse-integer (uiop:getenv "PORT") :junk-allowed t)))
+
+;; Say hello when site is loaded
+(hunchentoot:define-easy-handler (hello :uri "/") (name)
+  (setf (hunchentoot:content-type*) "text/plain")
+  (format nil "Hello~@[ ~A~]! ~% It works! Enjoy !!" name))
+
 (loop (sleep 1000))
